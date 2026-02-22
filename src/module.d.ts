009811c4f7341f961053e236c018b79941763a77
Shy <shyguyx1412@gmail.com>
2026-02-22 14:55:10 +0100

    setup project to pass typecheck

--- src/module.d.ts
+++ src/module.d.ts
@@ -2,3 +2,10 @@ declare module 'meta:api*' {
     const route: string;
     export default route;
 }
+
+//plugins only affect the language server
+//so this is needed for tsc
+declare module '*.module.css' {
+    const style: Record<string, string>;
+    export default style;
+}
